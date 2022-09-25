bcb205142482bb3372c282dcef4bcca7626b4a85
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-09-25 12:05:03 -0300

    Bootcamp Pottencial DIO
    Fazendo validações no set antes de atribuir o valor ao get

--- Bootcamps/PottencialDotNetDeveloper/Aulas/Pessoa.cs
+++ Bootcamps/PottencialDotNetDeveloper/Aulas/Pessoa.cs
@@ -1,18 +1,44 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-// namespace Aulas
-// {
-//     public class Pessoa
-//     {
-//         public string Nome {get; set;}
-//         public int Idade {get; set;}
-//         public void Apresentar()
-//         {
-//             Console.WriteLine($"Olá meu nome é {Nome}, eu tenho {Idade} anos");
-//         }
+namespace Aulas
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+        public string Nome 
+        {
+                get => _nome;
+                set
+                {
+                        if (value == "")
+                        {
+                                throw new ArgumentException("Você não informou seu nome.");
+                        }
+                        _nome = value;
+                }
+        }
+        public int Idade 
+        {
+                get => _idade ;
+                set
+                {
+                        if (value < 0)
+                        {
+                                throw new ArgumentException("idade não pode ser negativa.");
+                        }
+                        _idade = value;
+                }
+        }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é {Nome}, eu tenho {Idade} anos".ToUpper());
+        }
+
+        //MANEIRA DE INSTANCIAR A CLASSE PESSOA NO MESMO ARQUIVO.
         // static void Main(string[] args)
         // {
         //     Pessoa p1 = new Pessoa();
@@ -20,5 +46,5 @@
         //     p1.Idade = 32;
         //     p1.Apresentar();
         // }
-//     }
-// }
+    }
+}
