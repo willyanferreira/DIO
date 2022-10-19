e37f7ddc15417b8b3a9716ec02835aeda23fab83
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-18 23:24:24 -0300

    Update Program.cs
    Encapsulamento

--- Bootcamps/PottencialDotNetDeveloper/POO/Program.cs
+++ Bootcamps/PottencialDotNetDeveloper/POO/Program.cs
@@ -5,4 +5,8 @@ Pessoa p1 = new Pessoa();
 p1.Nome = "Willyan";
 p1.Idade = 33;
 
-p1.Apresentar();
+// p1.Apresentar();
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.Sacar(1289);
+c1.ExibirSaldo();
