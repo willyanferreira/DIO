0724aaed6d7eebeb0cca4ec49a9d88e54769c1b9
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-20 20:52:48 -0300

    Create Aluno.cs

---
+++ Bootcamps/PottencialDotNetDeveloper/POO/Models/Aluno.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO.Models
+{
+    public class Aluno : Pessoa // para fazer uma classe herdar de outra
+    {
+        public double Nota { get; set; }
+    }
+}
