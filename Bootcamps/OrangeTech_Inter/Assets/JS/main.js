ad21ad5678eee7e6c9d535188992792be66a903d
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-23 14:26:50 -0300

    acessando a url de cada pokémon para recuperar os detalhes

--- Bootcamps/OrangeTech_Inter/Assets/JS/main.js
+++ Bootcamps/OrangeTech_Inter/Assets/JS/main.js
@@ -1,11 +1,18 @@
+function pokemonsTypes(pokemonType){
+    return pokemonType.map((typeSlot) => `<li>${typeSlot.type.name}</li>`)
+}
 function listaPokemons(pokemon) {
     return `
         <li>
+            <span>${pokemon.order}</span>
             <span>${pokemon.name}</span>
+            <div>
+                <ol>${pokemonsTypes(pokemon.types)}</ol>
+            </div>
         </li>
     `
 }
 
 const listaPokemon = document.getElementById("listaPokemon");
 
-pokeAPI.getPkemons().then((pokemons = []) => listaPokemon.innerHTML += pokemons.map(listaPokemons).join(""))
+pokeAPI.getPokemons(0, 1).then((pokemons = []) => listaPokemon.innerHTML += pokemons.map(listaPokemons).join(""))
--- Bootcamps/OrangeTech_Inter/Assets/JS/pokeAPI.js
+++ Bootcamps/OrangeTech_Inter/Assets/JS/pokeAPI.js
@@ -1,8 +1,14 @@
 const pokeAPI = {}
-pokeAPI.getPkemons = (offset = 0, limit = 10) => {
+pokeAPI.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+pokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json()) //Quando usar Arrow Function em apenas uma linha não precisa usar o return. 
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonsDetails))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetail) => pokemonDetail)
         .catch((error) => console.log(error))
 }
