b44253fdb0c53e7a3799911902f62038f218285c
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-26 07:44:49 -0300

    pokédex

--- Bootcamps/OrangeTech_Inter/Pokedex/Assets/JS/main.js
+++ Bootcamps/OrangeTech_Inter/Pokedex/Assets/JS/main.js
@@ -1,14 +1,11 @@
-function pokemonsTypes(pokemonType){
-    return pokemonType.map((typeSlot) => `<li>${typeSlot.type.name}</li>`)
-}
 function listaPokemons(pokemon) {
     return `
-        <li>
-            <span>${pokemon.order}</span>
+        <li class="${pokemon.type}">
+            <span>${pokemon.number}</span>
             <span>${pokemon.name}</span>
             <div>
-                <ol>${pokemonsTypes(pokemon.types)}</ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}">
+            <ol>${pokemon.types.map((type) => `<li>${type}</li>`).join('')}</ol>
+                <img src="${pokemon.photo}">
             </div>
         </li>
     `
@@ -16,4 +13,4 @@ function listaPokemons(pokemon) {
 
 const listaPokemon = document.getElementById("listaPokemon");
 
-pokeAPI.getPokemons(0, 1).then((pokemons = []) => listaPokemon.innerHTML += pokemons.map(listaPokemons).join(""))
+pokeAPI.getPokemons().then((pokemons = []) => listaPokemon.innerHTML += pokemons.map(listaPokemons).join(""))
--- Bootcamps/OrangeTech_Inter/Pokedex/Assets/JS/pokeAPI.js
+++ Bootcamps/OrangeTech_Inter/Pokedex/Assets/JS/pokeAPI.js
@@ -1,7 +1,23 @@
 const pokeAPI = {}
-pokeAPI.getPokemonsDetails = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+
+function ourModelPokemon(pokedetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokedetail.order;
+    pokemon.name = pokedetail.name;
+    const types = pokedetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokedetail.sprites.other.dream_world.front_default;
+    return pokemon;
 }
+
+pokeAPI.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(ourModelPokemon)
+}
+
 pokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
