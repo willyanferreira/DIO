4fa6083c7f1f0baadffe9969f608866026ab52cb
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-29 16:37:28 -0300

    styled components

--- Bootcamps/OrangeTech_Inter/primeirospassosreact/src/pages/home/index.jsx
+++ Bootcamps/OrangeTech_Inter/primeirospassosreact/src/pages/home/index.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
+import { Button } from "../../components/button"
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <Button title="Home" />
             <button><Link to="/login">Fazer login</Link></button>
         </>
     )
--- Bootcamps/OrangeTech_Inter/primeirospassosreact/src/pages/login/index.jsx
+++ Bootcamps/OrangeTech_Inter/primeirospassosreact/src/pages/login/index.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
+import { Button } from "../../components/button"
+
 const Login = () => {
     return (
         <>
             <h1>Login</h1>
+            <Button title="Login" variant="secondary" />
             <button><Link to="/">Voltar para home</Link></button>
         </>
     )
