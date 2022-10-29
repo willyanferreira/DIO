9763d04010c63c1d42115757f4eb993c6b39f99c
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-10-29 14:03:27 -0300

    removendo alguns arquivos q n serão utilizados

--- Bootcamps/OrangeTech_Inter/primeirospassosreact/src/App.js
+++ Bootcamps/OrangeTech_Inter/primeirospassosreact/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá, <code>Willyan,</code> have a nice day!.
         </p>
         <a
           className="App-link"
