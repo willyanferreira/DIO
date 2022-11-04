b7279dbd63090076866462900c8d1616fe3ec022
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-11-03 23:59:31 -0300

    meu toque no projeto

--- Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/components/Input/style.js
+++ Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/components/Input/style.js
@@ -3,21 +3,23 @@ import styled from "styled-components";
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #aaffaa;
+    background-color: #00aaf0;
     display: flex;
     align-items: center;
-    justify-content: flex-end;
+    justify-content: center;
     font-size: 24px;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto', sans-serif;     
 
     input {
-        width: 100%;
-        height: 75px;
+        width: 90%;
+        height: 55px;
         background-color: #aaffaa;
-        border: 0;
+        border: 1px solid #000;
         text-align: right;
         font-size: 24px;
         font-family: 'Roboto', sans-serif;
         padding: 0 10px;
+        outline: none;
+        border-radius: 5px;
     }
 `
