e39fa2317b43ff729218264c3c78ae7d70b044a9
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-11-04 02:06:32 -0300

    Update index.jsx
    adicionando  a atributo  value ao input para receber os valores dos buttons

--- Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/components/Input/index.jsx
+++ Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/components/Input/index.jsx
@@ -1,9 +1,9 @@
 import { InputContainer } from "./style";
 
-export const  Input = () => {
+export const  Input = ({value}) => {
     return (
       <InputContainer>
-        <input disabled />
+        <input disabled value={value} />
       </InputContainer>
     );
   }
