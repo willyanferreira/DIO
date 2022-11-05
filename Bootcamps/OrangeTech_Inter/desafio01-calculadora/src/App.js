2aa71ee0c5f2a63253baefea1a9c732123e6d324
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-11-05 14:44:11 -0300

    quatro operações básicas funcionando

--- Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/App.js
+++ Bootcamps/OrangeTech_Inter/desafio01-calculadora/src/App.js
@@ -7,6 +7,8 @@ import { FaPowerOff } from 'react-icons/fa';
 
 export const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleAddNumber = (number) => {
     setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
@@ -14,6 +16,71 @@ export const App = () => {
 
   const handleOnClear = () => {
     setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+  }
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+');
+    }else{
+      const result = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(result)); 
+    }
+  }
+
+  const handleSubNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    }else{
+      const result = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(result));
+    }
+  }
+  const handleMulNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('*');
+    }else{
+      const result = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(result)); 
+    }
+  }
+  const handleDivNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    }else{
+      const result = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(result)); 
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleSubNumbers();
+          break;
+        case '*':
+          handleMulNumbers();
+          break;
+        case '/':
+          handleDivNumbers();
+          break;
+        default:
+          break;
+      }
+    }
   }
 
 
@@ -28,28 +95,28 @@ export const App = () => {
           <Button label={<FaPowerOff/>} className={"power"} />
         </Row>
         <Row>
-          <Button label={7} onClick={() => handleAddNumber('7')}/>
-          <Button label={8} onClick={() => handleAddNumber('8')}/>
-          <Button label={9} onClick={() => handleAddNumber('9')}/>
-          <Button label={"/"}/>
+          <Button label={'7'} onClick={() => handleAddNumber('7')}/>
+          <Button label={'8'} onClick={() => handleAddNumber('8')}/>
+          <Button label={'9'} onClick={() => handleAddNumber('9')}/>
+          <Button label={"/"} onClick={handleDivNumbers} />
         </Row>
         <Row>
-          <Button label={4} onClick={() => handleAddNumber('4')}/>
-          <Button label={5} onClick={() => handleAddNumber('5')}/>
-          <Button label={6} onClick={() => handleAddNumber('6')}/>
-          <Button label={"x"}/>
+          <Button label={'4'} onClick={() => handleAddNumber('4')}/>
+          <Button label={'5'} onClick={() => handleAddNumber('5')}/>
+          <Button label={'6'} onClick={() => handleAddNumber('6')}/>
+          <Button label={"x"} onClick={handleMulNumbers} />
         </Row>
         <Row>
-          <Button label={1} onClick={() => handleAddNumber('1')}/>
-          <Button label={2} onClick={() => handleAddNumber('2')}/>
-          <Button label={3} onClick={() => handleAddNumber('3')} />
-          <Button label={"-"}/>
+          <Button label={'1'} onClick={() => handleAddNumber('1')}/>
+          <Button label={'2'} onClick={() => handleAddNumber('2')}/>
+          <Button label={'3'} onClick={() => handleAddNumber('3')} />
+          <Button label={"-"} onClick={handleSubNumbers} />
         </Row>
         <Row>
           <Button label={","}/>
-          <Button label={0} onClick={() => handleAddNumber('0')}/>
-          <Button label={"+"}/>
-          <Button label={"="}/>
+          <Button label={'0'} onClick={() => handleAddNumber('0')}/>
+          <Button label={"+"} onClick={handleSumNumbers} />
+          <Button label={"="} onClick={handleEquals} />
         </Row>
       </Content>
     </Container>
